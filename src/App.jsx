1bae70f2d2f5ca0a1a2c1c08cf90cdc617ad3db9
Heather Amler <amler.heather@gmail.com>
2023-10-20 16:17:56 -0400

    Added dice roll component to view non-string prop values

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import './App.css'
 import Greeter from './Greeter';
+import Die from './Die';
 
 function App() {
   return (
     <div>
-      <Greeter person="Bill" from="Heather"/>
-      <Greeter person="Ted!" from="Will"/>
+      {/* <Greeter person="Bill" from="Heather"/>
+      <Greeter person="Ted!" from="Will"/> */}
+      <Die numSides={20}/>
+      <Die numSides={10}/>
+      <Die numSides={8}/>
+
     </div>
   ) 
 }
--- src/Greeter.jsx
+++ src/Greeter.jsx
@@ -3,7 +3,7 @@ export default function Greeter ({person, from}) {
     return (
         <>
             <h1>Hi there, {person}!</h1>
-            <h2>from, {from} </h2>
+            <h2>~from, {from} </h2>
         </>
     )
 }
