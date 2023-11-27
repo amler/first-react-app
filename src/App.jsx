5347e79f8c86a17d64be80124f4bff5936dd1a58
Heather Amler <amler.heather@gmail.com>
2023-11-27 16:55:53 -0500

    One eh way to do it

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function genRandomInt(max) {
 function CoreConcept (props) {
   return (
     <li>
-      <img src={props.img} alt={props.title} />
+      <img src={props.image} alt={props.title} />
       <h3>{props.title}</h3>
       <p>{props.description}</p>
     </li>
@@ -44,22 +44,19 @@ function App() {
             <CoreConcept 
               title={CORE_CONCEPTS[0].title}
               description={CORE_CONCEPTS[0].description}
-              img={CORE_CONCEPTS[0].image}
+              image={CORE_CONCEPTS[0].image}
             />
-            <CoreConcept 
-              title={CORE_CONCEPTS[1].title}
-              description={CORE_CONCEPTS[1].description}
-              img={CORE_CONCEPTS[1].image}
+            <CoreConcept {...CORE_CONCEPTS[0]}
             />
             <CoreConcept 
               title={CORE_CONCEPTS[2].title}
               description={CORE_CONCEPTS[2].description}
-              img={CORE_CONCEPTS[2].image}
+              image={CORE_CONCEPTS[2].image}
             />
             <CoreConcept 
               title={CORE_CONCEPTS[3].title}
               description={CORE_CONCEPTS[3].description}
-              img={CORE_CONCEPTS[3].image}
+              image={CORE_CONCEPTS[3].image}
             />
       
           </ul>
