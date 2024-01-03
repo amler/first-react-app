d2968d70ad84c1a39fea793db0d4378684b53877
Heather Amler <amler.heather@gmail.com>
2024-01-03 13:11:50 -0500

    Splitting to separate states

--- src/components/CoreConcepts.jsx
+++ src/components/CoreConcepts.jsx
@@ -1,9 +1,23 @@
-export default function CoreConcepts ({image, title, description}) {
+import { CORE_CONCEPTS } from '../data';
+import CoreConcept from './CoreConcept'; 
+
+export default function CoreConcepts () {
   return (
-    <li>
-      <img src={image} alt={title} />
-      <h3>{title}</h3>
-      <p>{description}</p>
-    </li>
+    <section id="core-concepts">
+      <h2>Time to get started!</h2>
+      <ul>
+        {CORE_CONCEPTS.map((conceptItem) => (
+          <CoreConcept key={conceptItem.title} {...conceptItem} />
+          ))}
+        {/* <CoreConcept 
+          title={CORE_CONCEPTS[0].title}
+          description={CORE_CONCEPTS[0].description}
+          image={CORE_CONCEPTS[0].image}
+        />
+        <CoreConcept {...CORE_CONCEPTS[1]}/>
+        <CoreConcept {...CORE_CONCEPTS[2]}/>
+        <CoreConcept {...CORE_CONCEPTS[3]}/> */}
+      </ul>
+    </section>
   );
-}   
+}
